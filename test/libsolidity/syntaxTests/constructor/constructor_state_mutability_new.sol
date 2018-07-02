f11197f4b64bacf0f9ae1e4dc546d46f9788b746
Erik Kundt <bitshift@posteo.org>
2018-07-02 17:47:48 +0200

    Adds default visibility specifier to syntax tests.

--- test/libsolidity/syntaxTests/constructor/constructor_state_mutability_new.sol
+++ test/libsolidity/syntaxTests/constructor/constructor_state_mutability_new.sol
@@ -1,13 +1,13 @@
 contract test1 {
-  constructor() constant {}
+  constructor() public constant {}
 }
 contract test2 {
-  constructor() view {}
+  constructor() public view {}
 }
 contract test3 {
-  constructor() pure {}
+  constructor() public pure {}
 }
 // ----
-// TypeError: (19-44): Constructor must be payable or non-payable, but is "view".
-// TypeError: (66-87): Constructor must be payable or non-payable, but is "view".
-// TypeError: (109-130): Constructor must be payable or non-payable, but is "pure".
+// TypeError: (19-51): Constructor must be payable or non-payable, but is "view".
+// TypeError: (73-101): Constructor must be payable or non-payable, but is "view".
+// TypeError: (123-151): Constructor must be payable or non-payable, but is "pure".
--- test/libsolidity/syntaxTests/constructor/constructor_state_mutability_old.sol
+++ test/libsolidity/syntaxTests/constructor/constructor_state_mutability_old.sol
@@ -1,16 +1,16 @@
 contract test1 {
-    function test1() constant {}
+    function test1() public constant {}
 }
 contract test2 {
-    function test2() view {}
+    function test2() public view {}
 }
 contract test3 {
-    function test3() pure {}
+    function test3() public pure {}
 }
 // ----
-// Warning: (21-49): Defining constructors as functions with the same name as the contract is deprecated. Use "constructor(...) { ... }" instead.
-// Warning: (73-97): Defining constructors as functions with the same name as the contract is deprecated. Use "constructor(...) { ... }" instead.
-// Warning: (121-145): Defining constructors as functions with the same name as the contract is deprecated. Use "constructor(...) { ... }" instead.
-// TypeError: (21-49): Constructor must be payable or non-payable, but is "view".
-// TypeError: (73-97): Constructor must be payable or non-payable, but is "view".
-// TypeError: (121-145): Constructor must be payable or non-payable, but is "pure".
+// Warning: (21-56): Defining constructors as functions with the same name as the contract is deprecated. Use "constructor(...) { ... }" instead.
+// Warning: (80-111): Defining constructors as functions with the same name as the contract is deprecated. Use "constructor(...) { ... }" instead.
+// Warning: (135-166): Defining constructors as functions with the same name as the contract is deprecated. Use "constructor(...) { ... }" instead.
+// TypeError: (21-56): Constructor must be payable or non-payable, but is "view".
+// TypeError: (80-111): Constructor must be payable or non-payable, but is "view".
+// TypeError: (135-166): Constructor must be payable or non-payable, but is "pure".
